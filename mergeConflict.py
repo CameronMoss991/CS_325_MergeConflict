4d061029b567ed722e83523ff07b40e68636e371
Cameron Moss <125421489+CameronMoss991@users.noreply.github.com>
2024-01-29 19:54:35 -0600

    Second commit

--- mergeConflict.py
+++ mergeConflict.py
@@ -12,7 +12,7 @@ elif what.lower() == "no":
     print("Correct, this program is a merge conflict example")
 elif what.lower() == "hello" or what.lower() == "hello world" or what.lower() == "hello world!!!":
     print("Hello World!!!")
-elif what.lower() == "word2" or what.lower() == "word": #Merge Conflict word
+elif what.lower() == "word1" or what.lower() == "word2" or what.lower() == "word3": #Merge Conflict word
     print("Funny")
 else:
     pass
